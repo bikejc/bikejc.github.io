5b866fda47bd8e5ea832a1e72257cf78e0bdb23f
Ryan Williams <ryan@runsascoded.com>
2022-04-03 18:35:00 -0400

    md jcast

--- pages/events/bike-jcast/bike-jcast-2017.tsx
+++ pages/events/bike-jcast/bike-jcast-2017.tsx
@@ -1,127 +1,61 @@
 import { Page } from "../../../src/page";
 
-    export default function Home() {
-        return (
-            <Page path={"/events/bike-jcast/bike-jcast-2017"}>
-                <p>
-             Friday, Oct. 13, we’ll hold a Very Special Light-up Night Ride ending at the opening party for the annual city-wide celebration of the arts, the Jersey City Art &amp; Studio Tour.
-             <br/>
-             About the official JCAST party: The Kick-Off will be from 7:00PM - 9:00 PM held at the JCAST 2017 Kick-off Night Event at the Mack Cali Atrium, 210 Hudson Street, Jersey City, NJ (near Exchange Place).
-            </p>
-            <p>
-             On Saturday, Oct. 14th and Sunday, Oct. 15th join Bike JC's
-             <strong>
-              "Bike To The Art"
-             </strong>
-             bicycle rides for the Jersey City Art Studio Tour JCAST!
-            </p>
-            <h3>
-             BikeJC JCAST Bike Tour Routes
-            </h3>
-            <p>
-             <strong>
-              ALL RIDES DEPART AND RETURN TO GROVE ST. PATH PLAZA!!!
-             </strong>
-             <br/>
-            </p>
-            <h4>
-             Friday 10/13 Light UP ride to Opening Night Party
-            </h4>
-            <p>
-             <br/>
-             <strong>
-              Saturday 12noon - 3pm Downtown Ride
-             </strong>
-             <br/>
-             - Art House 300 Coles St.
-             <br/>
-             - Hamilton House 255 Brunswick
-             <br/>
-             - Merseles Studio 339 Newark Ave.
-             <br/>
-             - Drawing Rooms 180 Grand St
-             <br/>
-             - The Oakman 160 1st St.)
-             <br/>
-             <br/>
-             <strong>
-              Saturday 3pm - 6pm Greenville, Bergen/Lafayette
-             </strong>
-             <br/>
-             - Project Greenville 128 Winfield Ave
-             <br/>
-             - Ocean Green Gallery 744 Ocean Ave
-             <br/>
-             - Pacific Flea Gallery
-             <br/>
-             - 149 Pacific Ave
-             <br/>
-             - The Artisits of 111 First Street
-             <br/>
-             - 395 Halladay St. #2
-             <br/>
-             - Deep Space Gallery 2nd Fl THE HIVE JC
-             <br/>
-             - 77 Cornelison Ave
-             <br/>
-             <br/>
-             <strong>
-              Sunday 12noon - 3pm Heights Ride
-             </strong>
-             <br/>
-             - Jim Fischer's Art Studio
-             <br/>
-             - 530 Liberty Ave
-             <br/>
-             - The Statuary 53 Congress St.
-             <br/>
-             - 107 Bowers Gallery and Art Space 107 Bowers St.
-             <br/>
-             - Pershing Field Community Center
-             <br/>
-             - 201 Central Ave. (enter from Summit Ave) - Musicians Building
-             <br/>
-             - 130 Central Ave
-             <br/>
-             - Prime Gallery
-             <br/>
-             - 351 Palisade Ave
-             <br/>
-             - Madill Studiio
-             <br/>
-             - 58 Laidlaw Ave
-             <br/>
-             <br/>
-             <strong>
-              Sunday 3pm - 6pm WEST / MCGINLEY Sq. / JSQ
-             </strong>
-             <br/>
-             - Independent Art World Gallery
-             <br/>
-             - 715 Newark Ave (John Ruddy &amp; Lillian Khan)
-             <br/>
-             - The Triangle Building - 2800 JFK Blvd, 1st Floor (37 artists)
-             <br/>
-             - El Cocotero
-             <br/>
-             - 749 Bergen Ave (Multiple Venezuelan artists)
-             <br/>
-             - Jersey City Bicycle Co
-             <br/>
-             - 687 Montgomery St (Chris De Los Angeles’ bike shop, work by our friend Daniel “Kodaq” Morteh &amp; 2 others, quick stop)
-             <br/>
-             - Shua Space - 340 Summit Ave (5 artists in small but cool space)
-             <br/>
-             - Dineen/Hull gallery @ HCCC
-             <br/>
-             - 71 Sip Ave (Kayt Hester, 1 other exhibit, closing party till 8)
-             <br/>
-             <br/>
-             <strong>
-              ALL RIDES DEPART AND RETURN TO GROVE ST. PATH PLAZA!!!
-             </strong>
-            </p>
-            </Page>
-        )
-    }
-    
+const md = `
+Friday, Oct. 13, we’ll hold a Very Special Light-up Night Ride ending at the opening party for the annual city-wide celebration of the arts, the Jersey City Art &amp; Studio Tour.
+
+About the official JCAST party: The Kick-Off will be from 7:00PM - 9:00 PM held at the JCAST 2017 Kick-off Night Event at the Mack Cali Atrium, 210 Hudson Street, Jersey City, NJ (near Exchange Place).
+
+On Saturday, Oct. 14th and Sunday, Oct. 15th join Bike JC's **"Bike To The Art"** bicycle rides for the Jersey City Art Studio Tour JCAST!
+
+### BikeJC JCAST Bike Tour Routes
+
+**ALL RIDES DEPART AND RETURN TO GROVE ST. PATH PLAZA!!!**
+
+#### Friday 10/13 Light UP ride to Opening Night Party
+
+**Saturday 12noon - 3pm Downtown Ride**
+- Art House 300 Coles St.
+- Hamilton House 255 Brunswick
+- Merseles Studio 339 Newark Ave.
+- Drawing Rooms 180 Grand St
+- The Oakman 160 1st St.)
+
+**Saturday 3pm - 6pm Greenville, Bergen/Lafayette**
+- Project Greenville 128 Winfield Ave
+- Ocean Green Gallery 744 Ocean Ave
+- Pacific Flea Gallery
+- 149 Pacific Ave
+- The Artisits of 111 First Street
+- 395 Halladay St. #2
+- Deep Space Gallery 2nd Fl THE HIVE JC
+- 77 Cornelison Ave
+
+**Sunday 12noon - 3pm Heights Ride**
+- Jim Fischer's Art Studio
+- 530 Liberty Ave
+- The Statuary 53 Congress St.
+- 107 Bowers Gallery and Art Space 107 Bowers St.
+- Pershing Field Community Center
+- 201 Central Ave. (enter from Summit Ave) - Musicians Building
+- 130 Central Ave
+- Prime Gallery
+- 351 Palisade Ave
+- Madill Studiio
+- 58 Laidlaw Ave
+
+**Sunday 3pm - 6pm WEST / MCGINLEY Sq. / JSQ**
+- Independent Art World Gallery
+- 715 Newark Ave (John Ruddy &amp; Lillian Khan)
+- The Triangle Building - 2800 JFK Blvd, 1st Floor (37 artists)
+- El Cocotero
+- 749 Bergen Ave (Multiple Venezuelan artists)
+- Jersey City Bicycle Co
+- 687 Montgomery St (Chris De Los Angeles’ bike shop, work by our friend Daniel “Kodaq” Morteh &amp; 2 others, quick stop)
+- Shua Space - 340 Summit Ave (5 artists in small but cool space)
+- Dineen/Hull gallery @ HCCC
+- 71 Sip Ave (Kayt Hester, 1 other exhibit, closing party till 8)
+
+**ALL RIDES DEPART AND RETURN TO GROVE ST. PATH PLAZA!!!**
+`
+
+export default function Home() { return <Page path={"/events/bike-jcast/bike-jcast-2017"} md={md} /> }
--- pages/events/bike-jcast/index.tsx
+++ pages/events/bike-jcast/index.tsx
@@ -1,27 +1,15 @@
 import { Page } from "../../../src/page";
 
-export default function Home() {
-    return (
-        <Page path={"/events/bike-jcast"}>
-            <p>
-                One of our newest events has quickly become a favorite:  “BIKEJCAST,” our free group bike tours in conjunction with city’s biggest annual arts event, the
-                <a href="http://thejcast.com/" target="_blank">
-                    Jersey City Art &amp; Studio Tour
-                </a>
-                (JCAST).
-            </p>
-            <p>
-                JCAST, held every October, is the culmination of the busy fall season in Jersey City’s growing arts community. Artists all over the city open their studios to thousands of visitors, special shows are mounted at galleries, there are music and theater events and galas―it’s a citywide arts buffet, with traditional and unconventional DIY venues all over the place, and official and unofficial parties and performances stretching into the wee hours.
-            </p>
-            <p>
-                Starting in 2014, we began offering bike tours on both full days of the festival, leading art-lovers on two wheels around some of the festival’s great venues. The tours are led by some of the same marshals who guide our big spring Ward Tour. But the pace is even mellower, with time to enjoy the art at every stop. We focus on some of the more far-flung venues that you might not get to on foot―including some of our city’s growing collection of outdoor murals, which are lots of fun to explore by bike! Note: Flamboyant decoration of both bikes and riders is encouraged!
-            </p>
-            <p>
-                Bike JC runs the BIKEJCAST tours in coordination with the Jersey City Office of Cultural Affairs (and the grace of the many artists involved). In future editions, we plan to work with them even more closely, to bring you more formal previews and maps of the venues on our routes in advance, and promote them to an even wider audience.
-            </p>
-            <p>
-                Join us and get your art on!
-            </p>
-        </Page>
-    )
-}
+const md = `
+One of our newest events has quickly become a favorite:  “BIKEJCAST,” our free group bike tours in conjunction with city’s biggest annual arts event, the [Jersey City Art & Studio Tour](https://thejcast.com/) (JCAST).
+
+JCAST, held every October, is the culmination of the busy fall season in Jersey City’s growing arts community. Artists all over the city open their studios to thousands of visitors, special shows are mounted at galleries, there are music and theater events and galas―it’s a citywide arts buffet, with traditional and unconventional DIY venues all over the place, and official and unofficial parties and performances stretching into the wee hours.
+
+Starting in 2014, we began offering bike tours on both full days of the festival, leading art-lovers on two wheels around some of the festival’s great venues. The tours are led by some of the same marshals who guide our big spring Ward Tour. But the pace is even mellower, with time to enjoy the art at every stop. We focus on some of the more far-flung venues that you might not get to on foot―including some of our city’s growing collection of outdoor murals, which are lots of fun to explore by bike! Note: Flamboyant decoration of both bikes and riders is encouraged!
+
+Bike JC runs the BIKEJCAST tours in coordination with the Jersey City Office of Cultural Affairs (and the grace of the many artists involved). In future editions, we plan to work with them even more closely, to bring you more formal previews and maps of the venues on our routes in advance, and promote them to an even wider audience.
+
+Join us and get your art on!
+`
+
+export default function Home() { return <Page path={"/events/bike-jcast"} md={md} /> }
