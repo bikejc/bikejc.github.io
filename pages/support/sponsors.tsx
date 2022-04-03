d2e13fe4479c68120ece97a90ce896953bee5856
Ryan Williams <ryan@runsascoded.com>
2022-04-03 08:53:39 -0400

    factor sponsors

--- pages/support/sponsors.tsx
+++ pages/support/sponsors.tsx
@@ -1,5 +1,46 @@
 import { Page } from "../../src/page";
 
+type Sponsor = {
+    href: string
+    src: string
+    alt: string
+}
+
+function Sponsors({ title, sponsors }: {
+    title: string
+    sponsors: Sponsor[]
+}) {
+    return (
+        <section className="block block-views block-views-sponsor-logos-all-block-3">
+            <h2 className="block-title">{title}</h2>
+            <div className="view view-sponsor-logos-all view-id-sponsor_logos_all view-display-id-block_3 sponsors">
+                <div className="view-content">
+                    <div className="item-list sponsors">
+                        <ul>{
+                            sponsors.map(sponsor => <Sponsor {...sponsor} />)
+                        }</ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function Sponsor({ href, src, alt, }: Sponsor) {
+    return (
+        <li>
+            <div className="views-field views-field-field-sponsor-logo">
+                <div className="field-content">
+                    <a href={href} target="_blank">
+                        <img alt={alt} height="400"
+                             src={src} typeof="foaf:Image" width="400"></img>
+                    </a>
+                </div>
+            </div>
+        </li>
+    )
+}
+
 export default function Home() {
     return (
         <Page path={"/support/sponsors"} documentClasses={["section-support"]}>
@@ -11,185 +52,40 @@ export default function Home() {
                     To find out ways your business can partner with Bike JC and help support our mission, please contact us.
                 </p>
             </div>
-            <section className="block block-views block-views-sponsor-logos-all-block-3">
-                <h2 className="block-title">Presenting Sponsor</h2>
-                <div className="view view-sponsor-logos-all view-id-sponsor_logos_all view-display-id-block_3 sponsors view-dom-id-6a60bb2ff5d906b50159974a6a812034">
-                    <div className="view-content">
-                        <div className="item-list sponsors">
-                            <ul>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="https://www.citibikejc.com/" target="_blank">
-                                                <img alt="Citibike Jersey City" height="400"
-                                                     src="/files/citibike-jc.png" typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="https://www.lyft.com/" target="_blank">
-                                                <img alt="lyft logo" height="400" src="/files/lyft-logo.jpg"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="block block-views block-views-sponsor-logos-all-block-4">
-                <h2 className="block-title">Founding Wheel Sponsors</h2>
-                <div className="view view-sponsor-logos-all view-id-sponsor_logos_all view-display-id-block_4 sponsors view-dom-id-0de8bbc1eceaf6994f4b10bcc91e8e0a">
-                    <div className="view-content">
-                        <div className="item-list sponsors">
-                            <ul>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://grovestreetbicycles.com/" target="_blank">
-                                                <img alt="Grove Street Bicycles" height="400"
-                                                     src="/files/grove-street-bicycles-giant.png" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.hudsontma.org/" target="_blank">
-                                                <img alt="Hudson TMA" height="400" src="/files/hudson-tma.png"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.cityofjerseycity.com/" target="_blank">
-                                                <img alt="City of Jersey City" height="400"
-                                                     src="/files/city-of-jersey-city.png" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://jerseycityculture.org/" target="_blank">
-                                                <img alt="Jersey City Office of Cultural Affairs" height="400"
-                                                     src="/files/cultural-affairs.jpg" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="https://www.li.me/electric-scooter" target="_blank">
-                                                <img alt="Lime logo" height="400" src="/files/lime-logo.jpg"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="https://www.jump.com/" target="_blank">
-                                                <img alt="Jump logo" height="400" src="/files/jump-logo.jpg"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="block block-views block-views-sponsor-logos-all-block-2">
-                <h2 className="block-title">Founding Gear Sponsors</h2>
-                <div className="view view-sponsor-logos-all view-id-sponsor_logos_all view-display-id-block_2 sponsors view-dom-id-d3c28436a32e9639d5c3b2160c61313f">
-                    <div className="view-content">
-                        <div className="item-list sponsors">
-                            <ul>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.rockitdocket.com/" target="_blank">
-                                                <img alt="Rock-it Docket" height="400"
-                                                     src="/files/rockitdocket-black-and-white_0.gif" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.roadid.com/" target="_blank">
-                                                <img alt="Road ID" height="400" src="/files/road-id.png"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="https://www.bird.co/" target="_blank">
-                                                <img alt="Bird Logo" height="400" src="/files/bird-logo.jpg"
-                                                     typeof="foaf:Image" width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.letsshareameal.org/" target="_blank">
-                                                <img alt="Let's Share a Meal logo" height="400"
-                                                     src="/files/lets-share-a-meal-logo.jpg" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="block block-views block-views-sponsor-logos-all-block-1">
-                <h2 className="block-title">Friends Sponsors</h2>
-                <div className="view view-sponsor-logos-all view-id-sponsor_logos_all view-display-id-block_1 sponsors view-dom-id-e81bb531e090b9027708974b520cbf82">
-                    <div className="view-content">
-                        <div className="item-list sponsors">
-                            <ul>
-                                <li className="">
-                                    <div className="views-field views-field-field-sponsor-logo">
-                                        <div className="field-content">
-                                            <a href="http://www.mysuezwater.com/" target="_blank">
-                                                <img alt="Suez" height="400" src="/files/suez.png" typeof="foaf:Image"
-                                                     width="400"></img>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Sponsors
+                title="Presenting Sponsor"
+                sponsors={[
+                    { href: "https://www.citibikejc.com/", src: "/files/citibike-jc.png", alt: "Citibike Jersey City", },
+                    { href: "https://www.lyft.com/", src: "/files/lyft-logo.jpg", alt: "lyft logo", },
+                ]}
+            />
+            <Sponsors
+                title="Founding Wheel Sponsors"
+                sponsors={[
+                    { href: "https://www.grovestreetbicycles.com/", src: "/files/grove-street-bicycles-giant.png", alt: "Grove Street Bicycles", },
+                    { href: "http://www.hudsontma.org/", src: "/files/hudson-tma.png", alt: "Hudson TMA", },
+                    { href: "http://www.cityofjerseycity.com/", src: "/files/city-of-jersey-city.png", alt: "City of Jersey City", },
+                    { href: "http://jerseycityculture.org", src: "/files/cultural-affairs.jpg", alt: "Jersey City Office of Cultural Affairs", },
+                    { href: "https://www.li.me/electric-scooter", src: "/files/lime-logo.jpg", alt: "Lime logo", },
+                    { href: "https://www.jump.com/", src: "/files/jump-logo.jpg", alt: "Jump logo", },
+                    // { href: "", src: "", alt: "", },
+                ]}
+            />
+            <Sponsors
+                title="Founding Gear Sponsors"
+                sponsors={[
+                    { href: "http://www.rockitdocket.com", src: "/files/rockitdocket-black-and-white_0.gif", alt: "Rock-it Docket", },
+                    { href: "http://www.roadid.com", src: "/files/road-id.png", alt: "Road ID", },
+                    { href: "https://www.bird.co/", src: "/files/bird-logo.jpg", alt: "Bird Logo", },
+                    { href: "http://www.letsshareameal.org/", src: "/files/lets-share-a-meal-logo.jpg", alt: "Let's Share a Meal logo", },
+                ]}
+            />
+            <Sponsors
+                title="Friends Sponsors"
+                sponsors={[
+                    { href: "http://www.mysuezwater.com/", src: "/files/suez.png", alt: "Suez", },
+                ]}
+            />
         </Page>
     )
 }
