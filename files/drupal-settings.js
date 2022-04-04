5e2c98368c85c89ff48e3c4f34f98209f5a6c75d
ryan-williams <nobigdealstyle@gmail.com>
2022-04-04 02:37:43 +0000

    Deploying to gh-pages from @ bikejc/www@b546e9b8e127102dd5a345e283f49c932205de58 🚀

---
+++ files/drupal-settings.js
@@ -0,0 +1,24 @@
+jQuery.extend(
+    Drupal.settings, {
+        "basePath": "/",
+        "pathPrefix":"",
+        "ajaxPageState": {},
+        "nivo_slider": {
+            "effect": "fade",
+            "slices": 15,
+            "boxCols": 8,
+            "boxRows": 4,
+            "animSpeed": 800,
+            "pauseTime": 3000,
+            "startSlide": 0,
+            "directionNav": true,
+            "controlNav": false,
+            "controlNavThumbs": false,
+            "pauseOnHover": true,
+            "manualAdvance": false,
+            "prevText": "Prev",
+            "nextText": "Next",
+            "randomStart": true,
+        }
+    }
+);
