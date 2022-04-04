33098ea34f49731e963eafb09c935806c49f4581
Ryan Williams <ryan@runsascoded.com>
2022-04-03 23:31:33 -0400

    finish citibike 2018 port

--- pages/resources/citi-bike-usage-jersey-city-2018.tsx
+++ pages/resources/citi-bike-usage-jersey-city-2018.tsx
@@ -1,9 +1,9 @@
 import { Page } from "../../src/page";
 
 const md = `
-This report is a summary of Citi Bike usage in Jersey City for 2018. Starting <a href="/resources/citibikejc-2017">last year</a>, Bike JC has run its own annual analysis of the bike share system in order to provide information to the public and promote discussion of biking and Citi Bike in our community.
+This report is a summary of Citi Bike usage in Jersey City for 2018. Starting [last year](/resources/citibikejc-2017), Bike JC has run its own annual analysis of the bike share system in order to provide information to the public and promote discussion of biking and Citi Bike in our community.
 
-Citi Bike in Jersey City is an extension of the system operating in New York City. It is managed by the private, for profit corporation <a href="https://www.motivateco.com/where-we-do-it/new-york-ny/">Motivate</a> (acquired by Lyft) and <a href="https://www.citibikenyc.com/system-data">data</a>about all bike trips is publicly available via their website. There is currently no official reporting by Motivate or the city government about usage of the bike share program.
+Citi Bike in Jersey City is an extension of the system operating in New York City. It is managed by the private, for profit corporation [Motivate](https://www.motivateco.com/where-we-do-it/new-york-ny/) (acquired by Lyft) and [data](https://www.citibikenyc.com/system-data) about all bike trips is publicly available via their website. There is currently no official reporting by Motivate or the city government about usage of the bike share program.
 
 In the following sections, we will provide statistics and data visualizations on details such as the number of trips, demographics of riders, length of trips, and frequency of station and route usage. When relevant, we will also compare these figures with 2017 metrics.
 
@@ -20,7 +20,7 @@ The number of Citi Bike trips starting or ending in Jersey City has increased 19
 
 Biking follows a seasonal pattern and breaking down total number of trips by month will show if this increase is uniform or concentrated during certain periods. Both 2018 and 2017 data will be plotted for comparison.
 
-<img alt="" src="/files/total trips by month.png" style="width: 400px; height: 305px;" />
+![](/files/total%20trips%20by%20month.png)
 
 Visually, it is apparent the greatest number of trips occurred in May through October, peaking in August (with 44.5K trips). Not surprisingly, these months have the best weather for biking. Data on winter and early spring, still show an increase relative to last year, but smaller so.
 
@@ -45,7 +45,7 @@ So at any point in time, how busy is the Jersey City Citi Bike system? In last y
 
 We would expect that to still be the case. We can verify by plotting the average number of trips by day of the week and comparing the patterns. Both data from 2018 and 2017 will be plotted.
 
-<img alt="" src="/files/avg trips day of week.png" />
+![](/files/avg%20trips%20day%20of%20week.png)
 
 There is a similar shape in the data for both years, but the increase in trips for 2018 is apparent. Trips during the work week saw a 21% increase vs 2017, while weekend trips saw a 14% increase.
 
@@ -53,7 +53,7 @@ This difference is reflective of the use cases for bike share systems. During th
 
 This difference can be visualized by plotting average trips by hour and splitting the work week and weekend into different lines. To simplify the visualization, the chart will only have data for 2018.
 
-<img alt="" src="/files/avg trips time of day.png" />
+![](/files/avg%20trips%20time%20of%20day.png)
 
 Work days have two very sharp peaks right at prime commuting times, 8 AM (159 average trips per hour) and 6 PM (125 average trips per hour). Weekend data shows a more gradual rise and fall with a peak around 1 PM (60 average trips per hour). The work week actually sees lower usage than weekends between the hours of 10 AM through 4 PM.
 
@@ -67,13 +67,13 @@ Citi Bike operates via fixed docking stations where all bikes are picked up from
 
 Below is a table showing the number of Citi Bike stations in each ward of Jersey City (as of December 2018).
 
-<img alt="" src="/files/stations currenlty active.png" />
+![](/files/stations%20currenlty%20active.png)
 
 As noted earlier, no stations currently operate in Ward A. Of the 50 total stations 27 are in the more affluent, Downtown area (Downtown is primarily Ward E, but a portion sits in Ward F, where two of its stations are located). Another detail to note is that the total number of stations has not increased since 2017. Effectively they have just been moved between areas.
 
 Which stations get the most and least use? Below we will show a table with the total number of trips that started or ended at each station in 2018.
 
-<img alt="" src="/files/total station use.png" style="width: 693px; height: 592px;" />
+![](/files/total%20station%20use.png)
 
 Stations near a PATH train station all make up four of the top five stations, including Grove Street PATH, Exchange Place, Sip Ave, and Newport PATH. Amazingly, Grove Street PATH station makes up about a 25% of all trips city-wide. It served over twice as many trips as the number two station.
 
@@ -81,7 +81,7 @@ Other notable stations include Hamilton Park. A neighborhood that is located abo
 
 How has station use changed since 2017? Below is a chart with similar content, but ranked by the change in number of trips year over year. To ensure an equivalent comparison, this only includes at stations that were around for all 24 months between January 2017 and December 2018.
 
-<img alt="" src="/files/growth in station use.png" />
+![](/files/growth%20in%20station%20use.png)
 
 Grove Street PATH is also the station with the largest increase in the number of trips. 2018 had nearly 11K more than in 2017. Other PATH adjacent stations also grew year over year.
 
@@ -96,7 +96,7 @@ The first question is trying to determine, are the trips taken typically short o
 
 The following chart will be a histogram representing the distribution of trip times. The height of each bar represents the number of trips that fall within the time range on the x-axis. An example to help you interpret the data, the height of the first bar can be read as the number of trips that took more than one minute, but less than two minutes.
 
-<img alt="" src="/files/distrubtion of trip times.png" />
+![](/files/distrubtion%20of%20trip%20times.png)
 
 The distribution is concentrated on the left side of the chart, meaning most trips are shorter. The greatest proportion of trips lasts between 4 and 5 minutes and 42% of trips are 5 minutes or less. This is very similar to 2017. Longer trips do happen, but they are a small proportion. Only about 12% of trips are longer than 15 minutes.
 
@@ -104,7 +104,7 @@ There is a clear skew towards shorter trips. So which stations are riders travel
 
 The following table lists the 25 most common routes and the number of times each was traveled in 2018.
 
-<img alt="" src="/files/top 25 routes.png" />
+![](/files/top%2025%20routes.png)
 
 The key takeaway from this table is that all of the top 25 routes either start or end at a PATH station.
 
@@ -115,46 +115,45 @@ A few other items on the list are notable, including:
 
 Reorganizing this data to analyze growth provides another perspective. Below is a table that orders each route by the growth in trips in 2018 compared to 2017.
 
-<img alt="" src="/files/growth in route usage.png" />
+![](/files/growth%20in%20route%20usage.png)
 
 There is a slight skew towards stations that opened mid-2017. Their growth numbers are large because their total trip count for 2017 were low because they were only open for part of the year. Still, this table shows areas of growth for the system across the city.
 
 The route with the greatest growth is Harborside to Newport PATH, also in the top 25 routes overall. Other important routes include stations outside of the Downtown area, including Ward D (Oakland Ave , Leonard Gordon Park, and Pershing Field), Ward B (Astor Place), and Ward C (Journal   Square, Dey Street, and Sip Ave). Again this highlights the importance of paying attention to the city as a whole when it comes to where growth will come from.
 
-### Conclusions &amp; Questions
+### Conclusions & Questions
 
 While this analysis looks at the trends in Citi Bike usage, it cannot tell us much about important policy questions. These topics are important for our city to consider as we work to make it friendlier to bikes and public transit, while ensuring equity in service to all residents.
 
 Jersey City subsidizes Citi Bike for residents qualifying for public housing and SNAP benefits. How many residents that qualify are aware of this benefit? How many qualifying residents even have stations close to their homes?
 
-How can we improve road infrastructure to support cyclists (though better signage, barriers, or even <a href="https://vimeo.com/293870204">bike lanes</a>)?
+How can we improve road infrastructure to support cyclists (though better signage, barriers, or even [bike lanes](https://vimeo.com/293870204))?
 
-What would be the impact of other kinds of bike share systems such as <a href="https://www.citibikenyc.com/how-it-works/electric">electric</a> <a href="https://www.nj.com/hudson/2019/03/ubers-electric-bike-company-eyes-jersey-city.html">pedal-assist</a> bikes or <a href="https://ny.curbed.com/2018/7/27/17617782/bike-share-new-york-dockless-facts-information">dockless</a> bikes?
+What would be the impact of other kinds of bike share systems such as [electric](https://www.citibikenyc.com/how-it-works/electric) [pedal-assist](https://www.nj.com/hudson/2019/03/ubers-electric-bike-company-eyes-jersey-city.html) bikes or [dockless](https://ny.curbed.com/2018/7/27/17617782/bike-share-new-york-dockless-facts-information) bikes?
 
 What changes could Citibike make to encourage more weekend usage/membership growth? Perhaps longer time limit for rides on Saturday and Sunday to allow more recreational cycling in places like Liberty State Park where no docks exist?
 
-To all readers, do you feel there were factors that play an important role in the analysis that we did not consider? Are there any problems with the analyses or conclusions in this article? Please share your thoughts and help us improve! Contact us at <a href="mailto:hello@bikejc.org">hello@bikejc.org</a> or on Twitter <a href="https://twitter.com/bikejc">@bikejc</a>.
+To all readers, do you feel there were factors that play an important role in the analysis that we did not consider? Are there any problems with the analyses or conclusions in this article? Please share your thoughts and help us improve! Contact us at [hello@bikejc.org](mailto:hello@bikejc.org) or on Twitter [@bikejc](https://twitter.com/bikejc).
 
 Data analysis performed by: Richard Trent
 
-Data source: <a href="https://s3.amazonaws.com/tripdata/index.html">Motivate</a>
+Data source: [Motivate](https://s3.amazonaws.com/tripdata/index.html)
 
-Analysis: <a href="https://github.com/rtrent/Jersey-City-Citi-Bike-2018">Github</a>
+Analysis: [Github](https://github.com/rtrent/Jersey-City-Citi-Bike-2018)
 
-Additional Tables: <a href="https://docs.google.com/spreadsheets/d/1DKcN7IWUCORYDP7jBALffMRNKCjQAuWquyc14qrU3k4/edit">Google Sheets</a>
+Additional Tables: [Google Sheets](https://docs.google.com/spreadsheets/d/1DKcN7IWUCORYDP7jBALffMRNKCjQAuWquyc14qrU3k4/edit)
 
-<img alt="" height="305" src="/files/total trips by month(1).png" typeof="foaf:Image" width="400"/>
-<img alt="" height="208" src="/files/trips age gender(1).png" typeof="foaf:Image" width="400"/>
-<img alt="" height="228" src="/files/stations currenlty active(1).png" typeof="foaf:Image" width="400"/>
-
-<img alt="" height="397" src="/files/avg trips day of week(1).png" typeof="foaf:Image" width="512"/>
-<img alt="" height="424" src="/files/avg trips time of day(1).png" typeof="foaf:Image" width="568"/>
-<img alt="" height="393" src="/files/distrubtion of trip times(1).png" typeof="foaf:Image" width="518"/>
-<img alt="" height="563" src="/files/growth in route usage(1).png" typeof="foaf:Image" width="594"/>
-<img alt="" height="368" src="/files/growth in station use(1).png" typeof="foaf:Image" width="700"/>
-<img alt="" height="568" src="/files/top 25 routes(1).png" typeof="foaf:Image" width="516"/>
-<img alt="" height="592" src="/files/total station use(1).png" typeof="foaf:Image" width="693"/>
-<img alt="" height="395" src="/files/total trips by month_0.png" typeof="foaf:Image" width="518"/>
+![](/files/total%20trips%20by%20month(1).png)
+![](/files/trips%20age%20gender(1).png)
+![](/files/stations%20currenlty%20active(1).png)
+![](/files/avg%20trips%20day%20of%20week(1).png)
+![](/files/avg%20trips%20time%20of%20day(1).png)
+![](/files/distrubtion%20of%20trip%20times(1).png)
+![](/files/growth%20in%20route%20usage(1).png)
+![](/files/growth%20in%20station%20use(1).png)
+![](/files/top%2025%20routes(1).png)
+![](/files/total%20station%20use(1).png)
+![](/files/total%20trips%20by%20month_0.png)
 `
 
 export default function Home() {
