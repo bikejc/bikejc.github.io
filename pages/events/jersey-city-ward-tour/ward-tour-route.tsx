d58456618c63db962a99484d0071f7d90405df49
Ryan Williams <ryan@runsascoded.com>
2022-04-19 17:38:53 -0400

    update ward-tour-route

--- pages/events/jersey-city-ward-tour/ward-tour-route.tsx
+++ pages/events/jersey-city-ward-tour/ward-tour-route.tsx
@@ -1,9 +1,15 @@
 import { Page } from "../../../src/page";
 
 const md = `
-**This is the new 2019 Ward Tour 10 map,** including a 15 mile route very similiar to last year's that stays in Jersey City (shown in Green) and the additional distance that includes both Bayonne and Hoboken for a 25 mile route (additional mileage shown in Purple). Both routes start from City Hall and end at the foot of Second Street near the Hudson River, at the Lutze Biergarten.
+[![Screenshot of Ward Tour route](/files/wt11-route-screenshot.png)](https://ridewithgps.com/routes/38744234)
 
-![Ward Tour Map](/files/Ward%20Tour%20Map%20for%20Digital.jpg)
+Full route map: https://ridewithgps.com/routes/38744234
+
+We all start at City Hall ([280 Grove St., at Montgomery Street](https://goo.gl/maps/tMHCxVZU4Q3qBGvc8)).
+
+We all end at [the Jersey City Jazz Festival](https://riverviewjazz.org/jersey-city-jazz-festival), at [107 Morgan St.](https://goo.gl/maps/YLfwHwet3ATYxVc79). Riders will find a bike valet and great food, drink, and entertainment options there!
+
+We’ll ride on streets (or sometimes on one side of a street) that are closed to motor-vehicle traffic by the Jersey City Police Department. Officers on bikes and in motor vehicles will escort us throughout the ride.
 `
 
 export default function Home() { return <Page path={"/events/jersey-city-ward-tour/ward-tour-route"} banner="/files/wardtourbanner1_4.jpg" md={md} /> }
