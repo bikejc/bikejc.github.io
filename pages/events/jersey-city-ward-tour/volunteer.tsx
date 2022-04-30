21c8424b4b470f10e230dd266bfd0e70744feb13
Ryan Williams <ryan@runsascoded.com>
2022-04-30 11:57:15 -0400

    update wt volunteer page

--- pages/events/jersey-city-ward-tour/volunteer.tsx
+++ pages/events/jersey-city-ward-tour/volunteer.tsx
@@ -1,63 +1,63 @@
 import { Page } from "../../../src/page";
 
 const md = `
-**WE NEED YOUR HELP TO RUN THE JERSEY CITY WARD TOUR!**
+#### WE NEED YOUR HELP TO RUN THE JERSEY CITY WARD TOUR! [SIGN UP TO VOLUNTEER HERE](https://forms.gle/oPshTHx44md5CBCH7).
 
-Rider registration is now open for this year's 10th Annual Ward Tour, rolling on **Sunday,  June 2, 2019**. We are in need of volunteers both leading up to the event and on the day of the tour. We can’t do it without you!
+[Rider registration is now open](https://www.eventbrite.com/e/jersey-city-ward-tour-2022-tickets-324066169637) for this year's 11th Annual Ward Tour, rolling on **Sunday, June 5, 2022**. We are in need of volunteers both leading up to the event and on the day of the tour. We can’t do it without you!
 
-**SHORT VERSION:**
-Some of our regular volunteers already know the Ward Tour drill by now from previous years. Please just save the date if you can work the event June 2, and know that we will soon schedule at least one meeting for volunteers to go over details and hand out whatever posters, postcards, and other promotional materials we have on hand, so you can help distribute them if you can.
+### SHORT VERSION
+We need volunteers to help with many tasks leading up to the event and on the day of the tour, including:
+- **Promoting the event**: putting up posters around the city, handing out palm cards, staffing tables at local events, etc.
+- **Starting line (8am-12pm):** registration/merch tables, misc setup
+- **During the ride (11am-2pm):** marshaling, wayfinding, SAG (Support And Gear)
+- **Finish line (2pm-7pm):** monitoring bike parking area, misc cleanup/teardown 
 
-Also, please **sign up at the bottom of this page,** a signup specifically for 2019 Ward Tour volunteers.
+In addition, we will be organizing bib pickups the day before the ride (Saturday, June 4), at various locations around Jersey City, and will need help organizing that process.
 
-Most of the Ward Tour and Festival will be very  similar to last year’s, with one important difference: A special longer route (25 miles) will include loops into Bayonne and Hoboken, at a somewhat faster pace (about 12 mph) and fewer stops. This group will start at 11 a.m., and will probably be smaller than the main group riding solely in JC (15 miles) starting a little later. This will mean that check-in is somewhat more complex. We will also need to identify some riding marshals who are comfortable with a somewhat longer and faster ride, to shepherd the 25-mile group.
+Many of our regular volunteers already know the Ward Tour drill from previous years. Please just save the date if you can work the event June 5, and know that we will soon schedule at least one meeting for volunteers to go over details and hand out whatever posters, postcards, and other promotional materials we have on hand, so you can help distribute them if you can.
 
-OK, we’ll be in touch soon via multiple emails, and thanks again!
+**Please [sign up here](https://forms.gle/oPshTHx44md5CBCH7) if you are interested in helping, and we'll be in touch to schedule a volunteers meeting in mid- to late-May.**
 
-**MORE DETAILS:**
+## VOLUNTEER NEEDS
 
-Ward Tour sign-in and bib pickup begin at 9:00 a.m. at City Hall, located at 280 Grove St. in downtown Jersey City. Riders will line up for the start at 10:45, and the first group  riders will roll promptly at 11:00 a.m., and end up around 2:00 p.m. at the finish line on the Hudson River waterfront along Second Street, in and around the Lutze Biergarten. Once there we'll continue to celebrate cycling all afternoon at our Finish Line Festival, featuring music, food and drinks, vendors and other attractions.
+### Before the Tour
 
-We will need LOTS of volunteers this year—probably more than ever, because we are planning to offer two somewhat different routes this year and will need ride marshals for both. (See above.)
+#### Promoting
+Leading up to the event, we need people to help promote the event:
+- putting up posters around the city
+- handing out palm cards
+- staffing our tables at local events like Groove on Grove
+- etc.
 
-**VOLUNTEER NEEDS:**
+#### Bib pickup day (Saturday, June 4)
+We'll have bibs available for riders to pick up at various locations around Jersey City the day before the event, and we'll need help organizing and staffing those locations.
 
-**Before the Tour:**
-
-Leading up to the event, our greatest need is for folks to help out with promoting the event, by putting up posters around the city, handing out palm cards, staffing our tables at local events like Groove on Grove, etc.
-
-**Day of the Tour:**
+### Day of the Tour
 
 Day-of roles that we need to fill are for setup of the start area and the festival finish area, rider registration, ride marshals, and staffing various booths and tables at the start and finish.
 
-**Setup:**
-Morning setup starts at 8 a.m. at City Hall and involves some heavy lifting and the ability to work fast.
+#### Setup
+Morning setup starts at **8 a.m. at City Hall** ([280 Grove St., at Montgomery Street](https://goo.gl/maps/tMHCxVZU4Q3qBGvc8)) and involves some heavy lifting and the ability to work fast.
 
-**Registration:**
-Morning pre-ride registration involves sitting at a table outside City Hall on the day of the event, and collecting completed registration forms, checking in pre-registered riders, and distributing numbered ride bibs. Registration usually starts at 9:00 a.m.
+#### Registration
+Morning pre-ride registration involves sitting at a table outside City Hall on the day of the event, and collecting completed registration forms, checking in pre-registered riders, and distributing numbered ride bibs. **Registration starts at 9:00 a.m.**
 
-**Marshals:**
-Marshals ride in strategic places within the tour, helping to direct other riders and keep them safe, help them with minor mechanical and other issues, communicate with police, and generally make the ride work smoothly. NOTE: We will need to find a few marshals willing and able to ride a little longer and faster than usual, for our special 25-mile optional route. Marshals should show up at City Hall in time to attend a briefing around 10:30 a.m.  along the south side of City Hall.
+#### Marshaling
+Marshals ride in strategic places within the tour, helping to direct other riders and keep them safe, help them with minor mechanical and other issues, communicate with police, and generally make the ride work smoothly. Marshals should show up at City Hall in time to attend a briefing around 10:30 a.m. along the south side of City Hall.
 
-**Festival Setup:**
-Setup workers will work in and around Lutze Biergarten from about noon to 2:00, while the riders are out on the tour. You will help set up the festival grounds. You will need to work quickly, follow a map/site plan for the festival, possibly do some lifting, and take initiative.
+#### Festival Setup
+Setup workers will work in and around the finish-line / [Jazz Festival](https://riverviewjazz.org/jersey-city-jazz-festival) **from about 12-2pm**, while the riders are out on the tour. You will help set up the festival grounds. You will need to work quickly, follow a map/site plan for the festival, possibly do some lifting, and take initiative.
 
-**NOTE:**
-THESE VOLUNTEERS CANNOT RIDE IN THE TOUR. If you are a friend of biking, but you can't or don't want to ride in the tour, please consider volunteering for this duty.
+**NOTE:** THESE VOLUNTEERS CANNOT RIDE IN THE TOUR. If you are a friend of biking, but you can't or don't want to ride in the tour, please consider volunteering for this duty.
 
-**Festival Staff:**
-Staff work begins later in the day in or around Lutze Biergarten and involves manning the t-shirt sales tables, helpers for the art, music, and children's areas, directing crowd flow, etc. We also need a few people to do similar jobs at the start at City Hall before the ride. The start and finish sites are only 5 minutes' walk from each other, so you could work at both sites.
+#### Finish Festival Staff
+Staff work begins later in the day in or around the finish line / [Jazz Festival](https://riverviewjazz.org/jersey-city-jazz-festival) ([107 Morgan St.](https://goo.gl/maps/YLfwHwet3ATYxVc79)), and involves staffing the t-shirt sales tables, monitoring the bike parking area, directing crowd flow, etc. We also need a few people to do similar jobs at the start at City Hall before the ride. The start and finish sites are only 5 minutes' walk from each other, so you could work at both sites.
 
-OK! If you can volunteer, **please provide a valid email address etc. at the bottom of this page,** a signup specifically for 2019 Ward Tour volunteers.
-
-All  communication will be handled via email.
-
-**IMPORTANT:**
-Apologies, but due to the rigors of putting on this big event,  we sometimes don’t have the capacity to answer detailed questions from individual volunteers on a day-to-day basis. We ask that you try to try to be self-sufficient before and during the event whenever possible; **when in doubt, just show up, ask other volunteers who know the drill, or find someone in charge.** We’ll put you to work as needed!
-
-Volunteers will be asked to sign a waiver form at the event sites.  You can view and download a  pdf of the waiver form [here](/file/bike-jc-volunteer-applicationpdf) if you like.
+All communication will be handled via email. Volunteers will be asked to sign a waiver form at event sites.
 
 Thanks again for helping us make this an amazing annual event!
+
+#### [SIGN UP TO VOLUNTEER HERE](https://forms.gle/oPshTHx44md5CBCH7).
 `
 
 export default function Home() { return <Page path={"/events/jersey-city-ward-tour/volunteer"} banner="/files/wardtourbanner1_2.jpg" md={md} /> }
